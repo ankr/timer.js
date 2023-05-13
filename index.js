f06edf7efbc4f945df093a697cc13bc53295862f
Andreas Kristiansen <mail@ankr.dk>
2023-05-13 15:49:18 +0200

    Add index.js exposing Timer

---
+++ index.js
@@ -0,0 +1,1 @@
+export { Timer } from './src/Timer.js';
--- src/Timer.test.js
+++ src/Timer.test.js
@@ -1,11 +1,11 @@
 import { describe, it } from 'node:test';
 import { equal } from 'node:assert/strict';
 
-import { Timer } from './Timer.js';
+import { Timer } from '../index.js';
 
 describe('Timer', () => {
-  describe('when accessing isStarted property', () => {
-    it('returns correct value', () => {
+  describe("isStarted property", () => {
+    it("returns correct value", () => {
       const timer = new Timer();
 
       equal(timer.isStarted, false);
@@ -13,6 +13,22 @@ describe('Timer', () => {
       timer.start();
 
       equal(timer.isStarted, true);
+    });
+  });
+
+  describe("isPaused property", () => {
+    it("returns correct value", () => {
+      const timer = new Timer();
+
+      equal(timer.isPaused, false);
+
+      timer.pause();
+
+      equal(timer.isPaused, true);
+
+      timer.resume();
+
+      equal(timer.isPaused, false);
     });
   });
 });
