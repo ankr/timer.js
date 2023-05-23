0828bbfbafcf426377c248958c84289baf584ce3
Andreas Kristiansen <mail@ankr.dk>
2023-05-23 11:37:01 +0200

    Add JSDoc

--- src/Timer.js
+++ src/Timer.js
@@ -1,17 +1,55 @@
+/**
+ * Timer class with pause functionality.
+ */
 export class Timer {
-  // Timing
+  /**
+   * Time timer was started.
+   *
+   * @type {number}
+   */
   #startTime;
+
+  /**
+   * @type {boolean}
+   */
   #isStarted;
 
-  // Pausing
+  /**
+   * @type {boolean}
+   */
   #isPaused;
+
+  /**
+   * Time timer was paused.
+   *
+   * @type {number}
+   */
   #pausedAt;
+
+  /**
+   * Time timer was started.
+   *
+   * @type {number}
+   */
   #pausedTime;
 
-  // Exposed values
+  /**
+   * Milliseconds since last tick.
+   *
+   * @type {number}
+   */
   #deltaTime;
+
+  /**
+   * Time passed since timer was started.
+   *
+   * @type {number}
+   */
   #elapsedTime;
 
+  /**
+   * Constructor.
+   */
   constructor() {
     this.#startTime = 0;
     this.#isStarted = false;
@@ -23,7 +61,9 @@ export class Timer {
   }
 
   /**
-   * Amount of time since last tick.
+   * Milliseconds passed since last tick.
+   *
+   * @return {number}
    */
   get deltaTime() {
     if (!this.#isPaused) {
@@ -33,29 +73,56 @@ export class Timer {
     return 0;
   }
 
+  /**
+   * Seconds passed since last tick.
+   *
+   * @return {number}
+   */
   get deltaSeconds() {
     return this.deltaTime / 1000;
   }
 
   /**
-   * Total amount of time since the timer was started.
+   * Milliseconds passed since timer was started.
+   *
+   * @return {number}
    */
   get elapsedTime() {
     return this.#elapsedTime;
   }
 
+  /**
+   * Seconds passed since timer was started.
+   *
+   * @return {number}
+   */
   get elapsedSeconds() {
     return this.elapsedTime / 1000;
   }
 
+  /**
+   * Tells whether the timer is started.
+   *
+   * @return {boolean}
+   */
   get isStarted() {
     return this.#isStarted;
   }
 
+  /**
+   * Tells whether the timer is paused.
+   *
+   * @return {boolean}
+   */
   get isPaused() {
     return this.#isPaused;
   }
 
+  /**
+   * Start the timer.
+   *
+   * @return {void}
+   */
   start() {
     if (!this.#isStarted) {
       this.#startTime = performance.now();
@@ -63,6 +130,11 @@ export class Timer {
     }
   }
 
+  /**
+   * Pause the timer.
+   *
+   * @return {void}
+   */
   pause() {
     if (!this.#isPaused) {
       this.#pausedAt = performance.now();
@@ -70,6 +142,11 @@ export class Timer {
     }
   }
 
+  /**
+   * Unpause the timer.
+   *
+   * @return {void}
+   */
   resume() {
     if (this.#isPaused) {
       this.#pausedTime += performance.now() - this.#pausedAt;
@@ -77,6 +154,11 @@ export class Timer {
     }
   }
 
+  /**
+   * Toggle pause on/off.
+   *
+   * @return {void}
+   */
   toggle() {
     if (this.#isPaused) {
       this.resume();
@@ -85,6 +167,13 @@ export class Timer {
     }
   }
 
+  /**
+   * Get the current time.
+   *
+   * This will respect the paused state of the timer.
+   *
+   * @return {void}
+   */
   now() {
     if (this.#isPaused) {
       return this.#pausedAt - this.#pausedTime;
@@ -93,6 +182,13 @@ export class Timer {
     return performance.now() - this.#pausedTime;
   }
 
+  /**
+   * Progress the timer one tick.
+   *
+   * This function should be executed inside the main loop.
+   *
+   * @return {void}
+   */
   tick() {
     const deltaTime = this.now() - this.#startTime - this.#elapsedTime;
     this.#elapsedTime += deltaTime;
